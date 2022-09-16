3ec93b8b2b0734d4dfad9286feb978d5f043f18c
quantumish <freifeld.david@gmail.com>
2022-09-15 20:08:48 -0700

    Restructure code

--- diffgame.py
+++ diffgame.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from random import random
 from numba import jit
+from sympy import *
+from zope.interface import *
+from typing import Tuple, Callable, NewType, Optional
 
-l = 1
-
-# @jit(nopython=True)
+# Utility funcs
 def euler(y_0, deriv, x_range, step=0.001):
     eps = 1e-3
     ys = [y_0]
@@ -17,25 +18,69 @@ def euler(y_0, deriv, x_range, step=0.001):
         xs.append(xs[-1] + step)
     return xs, ys
 
-def alice_lipschitz_strat():
-    return random(), 0, 0, (0, random()), (0, random())
+# globals 
 
-def bob_naive_strat():
-    return lambda x: x
-    
+l = 1
 
-for _i in range(100):
-    a,b,c,p1,p2 = alice_lipschitz_strat()
-    f = bob_naive_strat()
-    deriv = lambda x,y: (f(x)-l*y)/(a+b*x+c*x**2)
-    try:
-        xs, ys = euler(p1[1], deriv, (p1[0], 1))    
-        plt.plot(xs, ys)
-        plt.show()
-    except:
-        pass
-    
-    # plt.scatter([params[3][0], params[4][0]], [params[3][1], params[4][1]])
-    # for p in zip(xs, ys):
-    #     if (abs(p[0] - params[4][0]) < 0.01) and (abs(p[1] - params[4][1]) < 0.01):
-    #         print("Bob succeeds")
+# interfaces
+
+
+Point = NewType("Point", Tuple[float, float])
+class AliceStrat(Interface):
+    def round1() -> Tuple[float, float, float, Point, Point]:
+        """Picks alpha, beta, gamma constants and the two points solutions can/must go through."""
+
+    def round3(f: Callable[[float], float]) -> Optional[Callable[[float], float]]:
+        """Picks a unique solution for the differential equation."""
+
+class BobStrat(Interface):
+    def round2(a: float, b: float, c: float, p1: Point, p2: Point) -> Optional[Tuple[Callable[[float], float], Callable[[float], float]]]:
+        """Chooses a real-valued function f(x) and finds a solution to the differential equation."""
+
+# strats
+
+@implementer(AliceStrat)
+class LipschitzAndPray():    
+    def round1():
+        """Hope that y(x) is not Lipschitz continuous at 0."""
+        return random(), 0, 0, (0, random()), (0, random())
+
+    def round3(f):
+        """Pray it doesn't get this far - if it does, give up."""
+        return None
+
+
+@implementer(BobStrat)
+class Analytic():
+    def round2(a,b,c,p1,p2):    
+        f = lambda x: 0    
+        eq = Eq(Derivative(y(x), x), (x-l*y(x)/(a+b*x+c)))
+        print(latex(dsolve(eq)))
+        
+# verifying code
+
+def verify_diffeq(y, deriv):
+    h = 0.00001
+    for x in np.arange(-100, 100): # FIXME, super naive
+        if (y(x+h) - y(x))/h != deriv(x,y):
+            return False
+    return True
+
+alice = LipschitzAndPray()
+bob = Analytic()
+
+num_games = 10
+bob_wins = [True] * num_games
+for i in range(num_games):
+    a,b,c,p1,p2 = alice.round1()
+    f, y = bob.round2()
+
+    deriv = lambda x,y: (f(x)-l*y(x))/(a+b*x+c**2)
+    if verify_diffeq(y, deriv):    
+        y2 = alice.round3(f)        
+        if verify_diffeq(y2, deriv):
+            bob_wins[i] = False
+    else:
+        bob_wins[i] = False
+
+print(bob_wins)
