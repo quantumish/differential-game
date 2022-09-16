d1522888ebe50356d6091fb3da0cecac039e87f8
quantumish <freifeld.david@gmail.com>
2022-09-16 14:12:30 -0700

    Add numerical strategies and utilities

--- diffgame.py
+++ diffgame.py
@@ -64,6 +64,15 @@ class LipschitzAndPray:
         """Pray it doesn't get this far - if it does, give up."""
         return None
 
+@implementer(AliceStrat)
+class Roots:
+    def round1():        
+        return 4, -4, 1, (1, 4), (3, 4)
+
+    def round3(f):
+        """Pray it doesn't get this far - if it does, give up."""
+        return None
+
 
 @implementer(AliceStrat)
 class Random:
@@ -73,11 +82,13 @@ class Random:
     def round3(f):
         return None
 
-
+# @implementer(AliceStrat)
+# class Discontinuity
+    
 @implementer(AliceStrat)
 class Rude:
     """Be mean."""
-
+    
     def round1():
         big = sys.float_info.max
         return big, big, big, (big, big), (big, big)
@@ -88,7 +99,10 @@ class Rude:
 
 # import traceback
 @implementer(BobStrat)
-class PrecomputedAnalytic:
+class PrecomputedAnalyticZero:
+    """Assumes a,b,c are nonzero and picks f(x) = 0 to make things simple.
+    Plugs into a precomputed analytical solution to get y(x).
+    """
     def round2(a, b, c, p1, p2):
         try:
             scary_term = lambda x: math.exp(
@@ -105,31 +119,68 @@ class PrecomputedAnalytic:
 
 @implementer(BobStrat)
 class Analytic:
-    def round2(a, b, c, p1, p2):
-        f = lambda x: 0
+    """Uses SymPy to solve the differential equation."""
+    def __init__(self, f):
+        self.f = f
+    
+    def round2(self, a, b, c, p1, p2):
         y = Function("y")
         sa, sb, sc, sl, sx = symbols("a b c l x")
-        eq = Eq(Derivative(y(sx), sx), (-sl * y(sx) / (sa + sb * sx + sc * sx**2)))
+        eq = Eq(Derivative(y(sx), sx), (self.f-sl * y(sx) / (sa + sb * sx + sc * sx**2)))
         eq = eq.subs({sa: a, sb: b, sc: c, sl: l})
         sol = dsolve(eq)
         k = p1[1]/sol.subs({Symbol("C1"): 1, sx: p1[0]}).rhs
-        sol = sol.subs({Symbol("C1"): k})         
-        # print(sol.rhs)
-        return f, lambda x: sol.subs(sx, x).rhs
+        sol = sol.subs({Symbol("C1"): k})
+        return lambda x: 0, lambda x: sol.subs(sx, x).rhs
 
+
+@implementer(BobStrat)
+class Euler:
+    def __init__(self, f):
+        self.f = f
+    
+    """Approximates a solution numerically using Euler's method."""
+    def round2(self,a,b,c,p1,p2):
+        try: 
+            deriv = lambda x, y: (self.f(x) - l * y) / (a + b * x + c * x**2)    
+            xs, ys = euler(p1[1], deriv, (p1[0], 1))
+        except:
+            return None
+
+        # print(ys)
+        def near_analytic(x):
+            for i in range(len(xs))[:-1]:                
+                if x >= xs[i] and x < xs[i+1]:
+                    return ys[i]            
+            return np.nan
+        
+        return self.f, near_analytic
 
 # verifying code
 
 
 def verify_diffeq(y, deriv, p1, p2):
+    """Verifies a potential solution.
+    - Checks that it satisfies the differential equation
+    - Checks that it passes through at least one of the points
+    """    
     # verify it satisfies the diffeq
-    h = 0.00001
-    for x in np.arange(0, 1, 0.01):  # FIXME, super naive
-        if abs((y(x + h) - y(x)) / h - deriv(x, y)) > 0.01:
-            # print(x, y(x), (y(x+h) - y(x))/h, deriv(x,y))
+    h = 0.001
+    for x in np.arange(p1[0], p2[0], 0.01): # FIXME, super naive
+        if abs((y(x + h) - y(x)) / h - deriv(x, y)) > 0.001:
+            # print((y(x+h) - y(x))/h, deriv(x,y))
             return None
 
-    return (y(p1[0]) == p1[1], y(p2[0]) == p2[1])
+    epsilon = 0.001
+    return (abs(y(p1[0])-p1[1]) < epsilon, abs(y(p2[0]) - p2[1]) < epsilon)
+
+
+def plot_sol_and_pts(y, p1, p2):
+    xs = np.arange(-.1, 10, 0.01)    
+    plt.plot(xs, list(map(y, xs)))
+    plt.scatter([p1[0], p2[0]], [p1[1], p2[1]])
+    # plt.xlim(-.1, 1.1)
+    plt.show()
 
 
 class Result(Enum):
@@ -140,32 +191,45 @@ class Result(Enum):
     ALICE_WIN = (5,)
 
 
-alice = Random
-bob = Analytic
+alice = Roots
+s_x = Symbol("x")
+bob = Analytic(0) # Euler(lambda x: 0)
 
 num_games = 100
 bob_wins = [Result.WIN] * num_games
 for i in tqdm(range(num_games)):
     a, b, c, p1, p2 = alice.round1()
+    if p1[0] <= 0 or p1[1] <= 0 or p2[0] <= 0 or p2[1] <= 0:
+        continue
+    
     bob_choice = bob.round2(a, b, c, p1, p2)
     if bob_choice == None:
         bob_wins[i] = Result.GIVEUP
         continue
     f, y = bob_choice
-    # xs = np.arange(-100, 100)
-    # plt.plot(xs, list(map(y, xs)))
-    # plt.show()
 
     deriv = lambda x, y: (f(x) - l * y(x)) / (a + b * x + c * x**2)
-    pts = verify_diffeq(y, deriv, p1, p2)
-    if pts == None:
+    pts = verify_diffeq(y, deriv, p1, p2)    
+    if pts is None:
         bob_wins[i] = Result.INVALID_EQ
+        try:
+            plot_sol_and_pts(y, p1, p2)            
+        except:
+            print("complex valued!!!")
+            pass
+
+    # TODO implement forcing Alice to go through the same points as Bob
     elif True in pts:
         y2 = alice.round3(f)
         if y2 is not None and verify_diffeq(y2, deriv, p1, p2):
             bob_wins[i] = Result.ALICE_WIN
     else:
         bob_wins[i] = Result.NO_POINT
+        # try:
+        #     plot_sol_and_pts(y, p1, p2)
+        # except:
+        #     print("complex valued!!!")
+        #     pass
 
 
 print(f"Bob wins {bob_wins.count(Result.WIN)/num_games * 100}% of the time!")
